3d9412dff6560d990bbcb4053c9da970082054b5
Fechinmitchell <fechinmitchell1996@gmail.com>
2024-10-09 18:58:55 +1100

    Add openai to backend

--- scorelect-backend/app.py
+++ scorelect-backend/app.py
@@ -7,8 +7,8 @@ from flask_talisman import Talisman
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
-import openai
-from openai import OpenAI, OpenAIError
+from openai import OpenAI
+
 
 
 # Load environment variables

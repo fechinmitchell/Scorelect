e35e9e071b1c48ebd3b7036b23cf0dd73e5646a8
Fechinmitchell <fechinmitchell1996@gmail.com>
2024-10-19 17:00:32 +1100

    Add Dataset Functionality in Saved Games

--- scorelect-frontend/src/HowTo.js
+++ scorelect-frontend/src/HowTo.js
@@ -105,7 +105,7 @@ const HowTo = () => {
 
   useEffect(() => {
     filterTutorials();
-    // eslint-disable-next-line react-hooks/exhaustive--deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value, searchTerm]);
 
   const handleChange = (event, newValue) => {
